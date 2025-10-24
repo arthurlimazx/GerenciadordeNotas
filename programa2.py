bad6c6024833a1dba0a67a55c912f8efc1d0c137
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-10-24 09:01:33 -0300

    questao resolvida

--- programa2.py
+++ programa2.py
@@ -1,5 +1,6 @@
 from funcoes import *
 
+geralAlunos=[]
 alunosNomes=[]
 
 print("PROGRAMA PRINCIPAL")
@@ -10,4 +11,20 @@ while True:
 
     escolha=int(input("Faça sua escolha: "))
     if escolha==1:
-        aluno=str(input("Digite o nome do aluno: "))
+        nomeAluno=str(input("Digite o nome do aluno: "))
+        alunosNomes.append(nomeAluno)
+        nota1=int(input(f"Agora digite a nota 1 do aluno {nomeAluno}: "))
+        nota2=int(input(f"Agora digite a nota 2 do aluno {nomeAluno}: "))
+        nota3=int(input(f"Agora digite a nota 3 do aluno {nomeAluno}: "))
+        listaNotas=[nota1,nota2,nota3]
+
+    elif escolha==2:
+        alunoMedia= calcular_media(listaNotas)
+        situacao=verificar_situacao(alunoMedia)
+        alunoRelatorio= [{nomeAluno: listaNotas},{"media": alunoMedia},{"situação":situacao}]
+        geralAlunos.append(alunoRelatorio)
+        print(geralAlunos)
+
+    elif escolha==0:
+        break
+        
