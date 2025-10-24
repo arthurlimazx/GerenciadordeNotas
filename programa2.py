d366bf027f2e17b400acf70ff43875afe90f58b6
EduardaAndrade03 <eduardaamaria2608@gmail.com>
2025-10-24 09:51:53 -0300

    atividade terminada

--- programa2.py
+++ programa2.py
@@ -1,15 +1,16 @@
 from funcoes import *
+import emoji
 
 geralAlunos=[]
 alunosNomes=[]
 
-print("PROGRAMA PRINCIPAL")
+print(emoji.emojize("PROGRAMA PRINCIPAL :Santa_Claus:"))
 while True:
     print("1 - Cadastrar alunos e notas")
     print("2 - Exibir relatório")
     print("0 - Sair")
 
-    escolha=int(input("Faça sua escolha: "))
+    escolha=int(input("Faça sua escolha : "))
     if escolha==1:
         nomeAluno=str(input("Digite o nome do aluno: "))
         alunosNomes.append(nomeAluno)
@@ -17,14 +18,13 @@ while True:
         nota2=int(input(f"Agora digite a nota 2 do aluno {nomeAluno}: "))
         nota3=int(input(f"Agora digite a nota 3 do aluno {nomeAluno}: "))
         listaNotas=[nota1,nota2,nota3]
-
-    elif escolha==2:
         alunoMedia= calcular_media(listaNotas)
         situacao=verificar_situacao(alunoMedia)
         alunoRelatorio= [{nomeAluno: listaNotas},{"media": alunoMedia},{"situação":situacao}]
         geralAlunos.append(alunoRelatorio)
+
+    elif escolha==2:
         print(geralAlunos)
 
     elif escolha==0:
         break
-        
